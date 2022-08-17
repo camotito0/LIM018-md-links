5933857c635b5bce57f64a4fc406b562fb82518c
camotito0 <cecilianallerena@gmail.com>
2022-08-17 11:05:12 -0500

    Añadiendo función validateLinks, install axios, uninstall request. fetch

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,7 @@
 const process = require('process');
 const fs = require('fs');
 const path = require('path');
+const axios = require('axios');
 
 // process.argv recibe argumentos en el proceso de node.js, en este caso guarda
 // 1° la ruta absoluta desde donde se inició el proceso de node,
@@ -38,7 +39,6 @@ const fileOrDirectory = (filePath) => {
 let paths = [] ;
 const extractingPaths = (filePath) => {
 	if (!fileOrDirectory(filePath) && extentionFile(filePath) === '.md') {
-		// const fileDta = fs.readFileSync(filePath, 'utf8');
 		paths = paths.concat(filePath);
 	} else if(fileOrDirectory(filePath)) {
 		const contentDir = fs.readdirSync(filePath);
@@ -52,6 +52,7 @@ const extractingPaths = (filePath) => {
 	return paths;
 }
 
+// función para extraer los links de las rutas
 const extractingLinks = (paths) => {
 	let array = [];
 	let oneMatch = /\[([^\[]+)\]\((.*)\)/;
@@ -67,9 +68,56 @@ const extractingLinks = (paths) => {
 				file: path
 			}
 		})
-		array.push(newArray);
+		array.push(newArray)
 	})
 	return array.flat();
 }
 
-extractingLinks(extractingPaths(data[2]))
+// función para validar los links
+const validateLinks = (arrayLinks) => {
+	arrayLinks.map((linkObj) => {
+		return axios.get(linkObj.href)
+		.then(response => {
+			arrayLinks = {
+				...linkObj,
+				status :  response.status ,
+				ok : response.status >= 100 && response.status <= 199 ? response.status :
+				response.status >= 200 && response.status <= 299 ? `ok` :
+				response.status >= 300 && response.status <= 399 ? `redirección` :
+				response.status >= 400 && response.status <= 499 ? `fail, error del cliente` :
+				response.status >= 500 && response.status <= 599 ? `error en el servidor` :
+				'something went wrong'
+			}
+			/* Respuestas informativas (100–199),
+			Respuestas satisfactorias (200–299),
+			Redirecciones (300–399),
+			Errores de los clientes (400–499),
+			y errores de los servidores (500–599).
+			// console.log(e) */
+		})
+		.catch(error => {
+			arrayLinks = {
+				...linkObj,
+				fail :  error.message ,
+			}
+			console.log(arrayLinks)
+		});
+	})
+	return arrayLinks;
+	// resolve all promise
+	// promise.all
+}
+
+// console.log(validateLinks(extractingLinks(extractingPaths(data[2]))))
+validateLinks(extractingLinks(extractingPaths(data[2])))
+
+//mdLinks('archivo', {options}) 
+module.exports = {
+	extentionFile,
+	thePathExist,
+	isAFullPath,
+	fileOrDirectory,
+	extractingPaths,
+	extractingLinks,
+	validateLinks
+}
