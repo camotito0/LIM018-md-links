ec854548886cc90fb0ff20b507cd77ef035166da
camotito0 <cecilianallerena@gmail.com>
2022-08-30 22:02:21 -0500

    Añadiendo test para el modulo utils.js, se modifico la función validateLinks

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-const process = require('process');
+//const process = require('process');
 const fs = require('fs');
 const path = require('path');
 const axios = require('axios');
@@ -7,30 +7,24 @@ const axios = require('axios');
 // 1° la ruta absoluta desde donde se inició el proceso de node,
 // 2° ruta absoluta del archivo que se está ejecutando (utils.js)
 // y el argumento extra que sería el path
-const data = [...process.argv];
+//const data = [...process.argv];
 
 // regex que encuentra todos los links de un archivo con sintaxis markdown.
 const regexLinks = /\[([^\[]+)\](\(.*\))/gm;
 
-const extentionFile = (file) => { return path.extname(file) }
+const extentionFile = (file) => path.extname(file);
 
 // fs.existsSync verificamos si la ruta pasada existe o no
-const thePathExist = (filePath) => { return fs.existsSync(filePath) }
+const thePathExist = (filePath) => fs.existsSync(filePath);
 
-const isAFullPath = (filePath) => {
-	// path.isAbsolute ve si el path introducido es un 'fullpath' (ruta completa), retorna un booleano
-	// si no es así entonces con path resolve sacamos la ruta actual del documento y lo unimos con la ruta que nos pasan
-	// console.log(path.isAbsolute(filePath) ? 'true' : 'nono')
-	return path.isAbsolute(filePath) ? filePath : path.resolve(filePath);
-}
+// path.isAbsolute ve si el path introducido es un 'fullpath' (ruta completa), retorna un booleano
+// si no es así entonces con path resolve sacamos la ruta actual del documento y lo unimos con la ruta que nos pasan
+const isAFullPath = (filePath) => path.isAbsolute(filePath) ? filePath : path.resolve(filePath);
 
-const fileOrDirectory = (filePath) => {
-	// verificaremos si la ruta es un archivo o directorio
-	// fs.statSync nos devuelve un objeto con información acerca de la ruta
-	// estaremos utilizando el método isDirectory
-	const dta = fs.statSync(filePath);
-	return dta.isDirectory()
-}
+// verificaremos si la ruta es un archivo o directorio
+// fs.statSync nos devuelve un objeto con información acerca de la ruta
+// estaremos utilizando el método isDirectory
+const fileOrDirectory = (filePath) => fs.statSync(filePath).isDirectory();
 
 let paths = [] ;
 const extractingPaths = (filePath) => {
@@ -40,7 +34,7 @@ const extractingPaths = (filePath) => {
 		const contentDir = fs.readdirSync(filePath);
 		contentDir.forEach((content) => {
 			const fullPath = path.join(filePath, content);
-			return !fileOrDirectory(fullPath) && extentionFile(fullPath) === '.md' ? paths.push(fullPath) : extractingLinks (fullPath);
+			return !fileOrDirectory(fullPath) && extentionFile(fullPath) === '.md' ? paths.push(fullPath) : prueba(fullPath);
 		})
 	} else {
 		console.log('no existe archivo o directorio')
@@ -83,12 +77,7 @@ const validateLinks = (arrayLinks) => {
 			return arrayLinks = {
 				...links,
 				status : response.status ,
-				message : response.status >= 100 && response.status <= 199 ? response.status :
-				response.status >= 200 && response.status <= 299 ? response.statusText :
-				response.status >= 300 && response.status <= 399 ? response.statusText :
-				response.status >= 400 && response.status <= 499 ? response.statusText :
-				response.status >= 500 && response.status <= 599 ? response.statusText :
-				'something went wrong'
+				message : 'OK'
 			}
 		})
 		.catch(() => {
--- test/utils.spec.js
+++ test/utils.spec.js
@@ -1,15 +1,156 @@
 const utils = require('../src/utils');
+const axios = require('axios');
+const theTestPath = './prueba/prueba.md';
 
 describe('utils.extentionFile()', () => {
     it('debería ser una función', () => {
       expect(typeof utils.extentionFile).toBe('function');
     });
-    it('debería mostrarse en consola la extensión del archivo', () => {
-        // spyOn recibe dos parametros, el objeto que contiene los métodos y el método a testear
-        const logSpy = jest.spyOn(console, 'log');
-        //utils.extentionFile('.../prueba/prueba.md')
-        console.log(utils.extentionFile('.../prueba/prueba.md'))
-        //console.log(logSpy.mock.calls[0][0])
-        expect(logSpy.mock.calls[0][0]).toEqual('.md')
+    it('debería mostrar la extensión del archivo', () => {
+      expect(utils.extentionFile(theTestPath)).toEqual('.md')
     });
+});
+
+describe('utils.thePathExist()', () => {
+    it('debería ser una función', () => {
+      expect(typeof utils.thePathExist).toBe('function');
+    });
+    it('debería retornar true si es que existe', () => {
+      expect(utils.thePathExist(theTestPath)).toBe(true);
+    });
+});
+
+describe('utils.isAFullPath()', () => {
+  it('debería ser una función', () => {
+    expect(typeof utils.isAFullPath).toBe('function');
+  });
+  it('debería resolver la ruta relativa a absoluta', () => {
+    const absolutePath = '/mnt/c/Users/USUARIO/LIM018-md-links/prueba/prueba.md';
+    expect(utils.isAFullPath(theTestPath)).toEqual(absolutePath)
+  });
+});
+
+describe('utils.fileOrDirectory()', () => {
+  it('debería ser una función', () => {
+    expect(typeof utils.fileOrDirectory).toBe('function');
+  });
+  it('debería retornar true si es un directorio', () => {
+    expect(utils.fileOrDirectory(theTestPath)).toBe(false)
+  });
+});
+
+describe('utils.extractingPaths()', () => {
+  it('debería ser una función', () => {
+    expect(typeof utils.extractingPaths).toBe('function');
+  });
+  it('debería retornar un array que contenga la ruta obtenida (test archivo)', () => {
+    const pathFile = './README.md';
+    const result =   [ './README.md' ]
+    expect(utils.extractingPaths(pathFile)).toEqual(result);
+  });
+  it('debería retornar un array que contenga las rutas obtenidas (test directorio)', () => {
+    const pathDirectory = './prueba';
+    const result = ['./README.md', "prueba/prueba.md", "prueba/prueba2.md"];
+    expect(utils.extractingPaths(pathDirectory)).toEqual(result)
+  });
+});
+
+describe('utils.extractingLinks()', () => {
+  it('debería ser una función', () => {
+    expect(typeof utils.extractingLinks).toBe('function');
+  });
+  it('debería recorrer el array de rutas y extraer los links', () => {
+    const arrayPaths = [ 'prueba/prueba.md', 'prueba/prueba2.md' ];
+    const result =  [
+      {
+        href: 'https://nodejs.org/es/about/',
+        text: 'Acerca de Node.js - Documentación oficial',
+        file: 'prueba/prueba.md'
+      },
+      {
+        href: 'https://nodejs.org/api/fs.html',
+        text: 'Node.js file system - Documentación oficial',
+        file: 'prueba/prueba.md'
+      },
+      {
+        href: 'https://nodejs.org/es/about/',
+        text: 'Acerca de Node.js - Documentación oficial',
+        file: 'prueba/prueba2.md'
+      },
+      {
+        href: 'https://nodejs.org/api/fs.html',
+        text: 'Node.js file system - Documentación oficial',
+        file: 'prueba/prueba2.md'
+      }
+    ]
+    expect(utils.extractingLinks(arrayPaths)).toEqual(result)
+  });
+});
+
+describe('utils.validateLinks()', () => {
+  const testArrayLinks = [
+    {
+      href: 'https://nodejs.org/api/fs.html',
+      text: 'Node.js file system - Documentación oficial',
+      file: '/mnt/c/Users/USUARIO/LIM018-md-links/prueba/prueba2.md'
+    }
+  ]
+
+  it('debería ser una función', () => {
+    expect(typeof utils.validateLinks).toBe('function');
+  });
+  it('debería resolverse al validar el href del objeto y devolver el objeto actualizado con estatus y message', () => {
+    // spyOn recibe dos parametros, el objeto que contiene los métodos y el método a testear
+    const getSpy = jest.spyOn(axios, 'get');
+    axios.get.mockImplementation((_link_) => Promise.resolve({
+      ...[testArrayLinks],
+      status: 200,
+      message:  'OK'
+    }))
+
+    const result = utils.validateLinks(testArrayLinks);
+    result
+    .then((link) => {
+      expect(link).toHaveLength(1);
+      expect(link[0].value.status).toEqual(200);
+      expect(link[0].value.message).toEqual('OK');
+    });
+
+  });
+
+  it('debería fallar al validar el href del objeto y devolver el objeto actualizado con estatus y message', () => {
+    const getSpy = jest.spyOn(axios, 'get');
+    axios.get.mockImplementation((_link_) => Promise.reject({
+      ...[testArrayLinks],
+      status: 'something went wrong',
+      message:  'FAIL'
+    }))
+
+    const result = utils.validateLinks(testArrayLinks);
+    result
+    .then((link) => {
+      expect(link).toHaveLength(1);
+      expect(link[0].value.status).toEqual('something went wrong');
+      expect(link[0].value.message).toEqual('FAIL');
+    });
+  });
+});
+
+describe('utils.statsLinks()', () => {
+  it('debería ser una función', () => {
+    expect(typeof utils.statsLinks).toBe('function');
+  });
+  it('debería devolver un array con las estidísticas', () => {
+    const testArrayLinks = [
+      {
+        href: 'https://nodejs.org/api/fs.html',
+        text: 'Node.js file system - Documentación oficial',
+        file: '/mnt/c/Users/USUARIO/LIM018-md-links/prueba/prueba2.md'
+      }
+    ]
+    let result = [];
+    result.total = 1;
+    result.unique = 1;
+    expect(utils.statsLinks(testArrayLinks)).toEqual(result);
+  });
 });
